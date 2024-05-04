9746d258216dbae87add0bab00bb1fd3339464d6
Mullins182 <Mullins182@t-online.de>
2024-05-04 10:51:09 +0200

    Made a Listbox and created new class to generate items (Textboxes) for Listbox in code-behind.

--- ToDoList-App/MainWindow.xaml.cs
+++ ToDoList-App/MainWindow.xaml.cs
@@ -8,14 +8,42 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ToDoList_App.Model;
 
 namespace ToDoList_App          // The name says everything :)
 {
     public partial class MainWindow : Window
     {
+        private string x = "Hi, I just want to know how to change a WPF label FontFamily in code-behind, since I have seen a lot of examples in C# but not In Basic. Thank you!";
+        private string y = "PS: I can't understand why these things were so much simpler in Windows Forms, and basic things like this are so hard to do in WPF.";
+        private string z = "Change Label FontFamily in code behind WPF Basic.PS: I can't understand why these things were so much simpler in Windows Forms, and basic things like this are so hard";
+
         public MainWindow()
         {
             InitializeComponent();
+
+            ToDoTextBox a = new();
+            ToDoTextBox b = new();
+            ToDoTextBox c = new();
+            ToDoTextBox d = new();
+            ToDoTextBox e = new();
+            ToDoTextBox f = new();
+            ToDoTextBox g = new();
+            ToDoTextBox h = new();
+            ToDoTextBox i = new();
+
+
+            ToDoList.Items.Add(a.ToDo(x, "Box1"));
+            ToDoList.Items.Add(b.ToDo(y, "Box2"));
+            ToDoList.Items.Add(c.ToDo(z, "Box3"));
+            ToDoList.Items.Add(d.ToDo(x, "Box1"));
+            ToDoList.Items.Add(e.ToDo(y, "Box2"));
+            ToDoList.Items.Add(f.ToDo(z, "Box3"));
+            ToDoList.Items.Add(g.ToDo(x, "Box1"));
+            ToDoList.Items.Add(h.ToDo(y, "Box2"));
+            ToDoList.Items.Add(i.ToDo(z, "Box3"));
+            ToDoList.Focus();
+
         }
 
 
---
+++ ToDoList-App/Model/ToDoTextBox.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace ToDoList_App.Model
+{
+    public class ToDoTextBox
+    {
+        private TextBox Box = new();
+
+        public ToDoTextBox() { }
+
+        public TextBox ToDo(string x, string boxName)
+        {
+            Box.TextWrapping = System.Windows.TextWrapping.Wrap;
+            Box.TextAlignment = System.Windows.TextAlignment.Justify;
+            Box.TextDecorations = System.Windows.TextDecorations.Baseline;
+            Box.FontFamily      = new FontFamily("Bradley Hand ITC");
+            Box.Name            = boxName;
+            Box.Foreground      = Brushes.DarkSlateGray;
+            Box.Background      = Brushes.Transparent;
+            Box.BorderThickness = new Thickness(0, 0, 0, 5);
+            Box.FontSize        = 40;
+
+            Box.Text            = x;
+
+            return Box;
+        }
+    }
+}
