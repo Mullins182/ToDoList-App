f49465c8bc9714e27fb343905eabcf7ad8a608af
Mullins182 <Mullins182@t-online.de>
2024-05-04 17:56:41 +0200

    changed text alignment for ToDoBoxes

--- ToDoList-App/Model/ToDoTextBox.cs
+++ ToDoList-App/Model/ToDoTextBox.cs
@@ -18,7 +18,7 @@ namespace ToDoList_App.Model
         public TextBox ToDo(string x, string boxName)
         {
             Box.TextWrapping = System.Windows.TextWrapping.Wrap;
-            Box.TextAlignment = System.Windows.TextAlignment.Justify;
+            Box.TextAlignment = System.Windows.TextAlignment.Center;
             //Box.TextDecorations = System.Windows.TextDecorations.Underline;
 
             Box.FontFamily      = new FontFamily("Bradley Hand ITC");
@@ -29,7 +29,7 @@ namespace ToDoList_App.Model
             Box.FontSize        = 40;
             Box.FontWeight      = FontWeights.Bold;
 
-            Box.Text            = $"{(char)1421} {x}";    // 1421 | 0x2610 = Ballot Box | 0x2713 = Check Mark
+            Box.Text            = $"{(char)1421}\n{x}";    // 1421 | 0x2610 = Ballot Box | 0x2713 = Check Mark
 
             return Box;
         }
