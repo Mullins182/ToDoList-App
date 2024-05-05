4bd295670ddebfbc721db9109f5ec0f629c4d16b
Mullins182 <Mullins182@t-online.de>
2024-05-05 21:29:03 +0200

    implemented animation and sound effects. Found Bug when adding more than three ToDo entrys ... Seems some entrys are duplicated when data gets read from file and put into Listbox. :(

--- ToDoList-App/MainWindow.xaml.cs
+++ ToDoList-App/MainWindow.xaml.cs
@@ -16,32 +16,44 @@ namespace ToDoList_App          // The name says everything :)
 {
     public partial class MainWindow : Window
     {
-        private readonly DispatcherTimer saveTimer  = new();
+        private readonly MediaPlayer floppyWrite            = new();
+
+        private readonly DispatcherTimer saveTimer          = new();
+        private readonly DispatcherTimer savingAnimTimer    = new();
 
         private List<TextBox> toDoEntrys            = [];
 
-        private Label SaveInfo                      = new();
+        private double savingAnimRectPos            = 0.00;
+        private bool savingAnimRectUp               = true;
+        private bool playbackEnded                  = true;
 
         private readonly string markInWorks         = $"{(char)1421} in the works {(char)1421}";    
         private readonly string markDone            = $"Done {(char)0x2713}";
 
         public MainWindow()
         {
+            floppyWrite.IsMuted = true;
+
             InitializeComponent();
 
+            InitializePrg();
+        }
+
+        private async void InitializePrg()
+        {
+            await Task.Delay(500);
+
+            floppyWrite.Open(new Uri("Sounds/readingFloppyDisc.mp3", UriKind.Relative));
+            floppyWrite.Position = TimeSpan.FromMilliseconds(50);
+
             saveTimer.Interval = TimeSpan.FromMinutes(1);
+            savingAnimTimer.Interval = TimeSpan.FromMilliseconds(25);
             saveTimer.Tick += SaveRoutine;
+            savingAnimTimer.Tick += SavingAnimationRoutine;
 
-            SaveInfo.Visibility = Visibility.Hidden;
-            SaveInfo.Width = 300;
-            SaveInfo.Height = 50;
-            SaveInfo.Foreground = Brushes.OrangeRed;
-            SaveInfo.FontFamily = new FontFamily("Chiller");
-            SaveInfo.Content = "Data Saved !";
-            SaveInfo.FontSize = 37;
-            SaveInfo.FontWeight = FontWeights.Bold;
-
-            Menu.Children.Add(SaveInfo);
+            Canvas.SetLeft(SavingAnim, (SavingCanvas.Width / 2) - SavingAnim.Width / 2);
+            Canvas.SetTop(SavingAnim, (SavingCanvas.Height / 2) - SavingAnim.Height / 2);
+            savingAnimRectPos = Canvas.GetTop(SavingAnim);
 
             ToDoList.ItemsSource = toDoEntrys;
 
@@ -53,15 +65,47 @@ namespace ToDoList_App          // The name says everything :)
             ToDoList.Focus();
 
             saveTimer.Start();
+
+            floppyWrite.IsMuted       = false;
+        }
+
+        private void SavingAnimationRoutine(object? sender, EventArgs e)
+        {
+            if (savingAnimRectPos < 30)
+            {
+                savingAnimRectUp = false;
+            }
+
+            if (savingAnimRectPos > 50)
+            {
+                savingAnimRectUp = true;
+            }
+
+            if (savingAnimRectUp)
+            {
+                Canvas.SetTop(SavingAnim, savingAnimRectPos -= 1.5);
+            }
+            else
+            {
+                Canvas.SetTop(SavingAnim, savingAnimRectPos += 1.5);
+            }
         }
 
         private async void SaveRoutine(object? sender, EventArgs e)
         {
-            SaveInfo.Visibility = Visibility.Visible;
+            savingAnimTimer.Start();
+            SavingAnim.Visibility = Visibility.Visible;
+
             SaveData();
 
+            floppyWrite.Position = TimeSpan.FromMilliseconds(50);
+            floppyWrite.Play();
+
             await Task.Delay(4500);
-            SaveInfo.Visibility = Visibility.Hidden;
+
+            floppyWrite.Stop();
+            savingAnimTimer.Stop();
+            SavingAnim.Visibility = Visibility.Hidden;
         }
 
         private void SaveData()
@@ -124,9 +168,31 @@ namespace ToDoList_App          // The name says everything :)
             ToDoList.Focus();
         }
 
-        private void Save_Click(object sender, RoutedEventArgs e)
+        private async void Save_Click(object sender, RoutedEventArgs e)
         {
-            SaveData();
+            if (playbackEnded)
+            {
+                saveTimer.Stop();
+                savingAnimTimer.Start();
+                SavingAnim.Visibility = Visibility.Visible;
+                playbackEnded = false;
+
+                floppyWrite.Position = TimeSpan.FromMilliseconds(50);
+                floppyWrite.Play();
+                SaveData();
+
+                await Task.Delay(4500);
+
+                floppyWrite.Stop();
+
+                savingAnimTimer.Stop();
+
+                SavingAnim.Visibility = Visibility.Hidden;
+
+                playbackEnded = true;
+
+                saveTimer.Start();
+            }
         }
 
         private void Exit_Click(object sender, RoutedEventArgs e)
@@ -136,32 +202,32 @@ namespace ToDoList_App          // The name says everything :)
         
         private void NewEntry_MouseEnter(object sender, MouseEventArgs e)
         {
-            NewEntry.Foreground = Brushes.YellowGreen;
+            NewEntry.Foreground = Brushes.DarkRed;
         }
 
         private void NewEntry_MouseLeave(object sender, MouseEventArgs e)
         {
-            NewEntry.Foreground = Brushes.OrangeRed;
+            NewEntry.Foreground = Brushes.Black;
         }
 
         private void Save_MouseEnter(object sender, MouseEventArgs e)
         {
-            Save.Foreground = Brushes.YellowGreen;
+            Save.Foreground = Brushes.DarkRed;
         }
 
         private void Save_MouseLeave(object sender, MouseEventArgs e)
         {
-            Save.Foreground = Brushes.OrangeRed;
+            Save.Foreground = Brushes.Black;
         }
 
         private void Exit_MouseEnter(object sender, MouseEventArgs e)
         {
-            Exit.Foreground = Brushes.YellowGreen;
+            Exit.Foreground = Brushes.DarkRed;
         }
 
         private void Exit_MouseLeave(object sender, MouseEventArgs e)
         {
-            Exit.Foreground = Brushes.OrangeRed;
+            Exit.Foreground = Brushes.Black;
         }
     }
 }
